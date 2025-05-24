3f88472d7290baf5f4ea64b5bdf89479c5168e51
yy933 <emilyyu025@gmail.com>
2025-05-24 18:38:06 +0800

    feat: add header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,35 @@
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[80px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
