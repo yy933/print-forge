70946c9e1c166dcfc04d9dfb254a8e064894c0e0
yy933 <emilyyu025@gmail.com>
2025-05-24 18:48:03 +0800

    feat: apply google fonts to the contents

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,26 @@
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
+});
 
 export default function RootLayout({
   children,
 }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${albertSans.className} ${montserratAlternates.variable}`}
+      >
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
             <div className="relative">
