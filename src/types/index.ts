917c8e5f11ce030748336fb5b3ab83fe9cd44e0e
yy933 <emilyyu025@gmail.com>
2025-05-27 00:24:08 +0800

    refactor: rename all .jsx files to .tsx

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+import type { ReactNode } from "react";
+
+// Data Types
+export type Model = {
+  id: number;
+  name: string;
+  description: string;
+  likes: number;
+  image: string;
+  category: string;
+  dateAdded: string;
+};
+
+// Page Types
+export type RootLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export type ModelDetailPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+// Components Types
+export type ModelCardProps = {
+  model: Model;
+};
+
+export type PillProps = {
+  children: ReactNode;
+  className?: string;
+};
