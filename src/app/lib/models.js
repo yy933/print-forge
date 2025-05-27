955f9a3b974b10a02c18dd8e425a76cbf0eb53ab
yy933 <emilyyu025@gmail.com>
2025-05-27 11:41:45 +0800

    refactor: add models.ts

--- src/app/lib/models.js
+++
@@ -1,7 +0,0 @@
-import modelsData from "../data/models.json";
-
-
-export async function getAllModels() {
-
-  return modelsData;
-}
--- src/app/lib/models.ts
+++ src/app/lib/models.ts
@@ -1,18 +1,12 @@
 import modelsData from "../data/models.json";
-import type { Model } from "../types";
+import type { Model } from "@/types";
 
 export async function getAllModels(): Promise<Model[]> {
-  // This is where you'd write code to fetch the list
-  // of models from a database. We're mocking that with
-  // our JSON array of data in models.json for now.
   return modelsData;
 }
 
 export async function getModelById(id: string | number): Promise<Model> {
-  // These functions don't technically need to be async functions,
-  // but we're planning for the future when they'll be fetching
-  // from a real data source.
-  const foundModel = modelsData.models.find(
+  const foundModel = modelsData.find(
     (model: Model) => model.id.toString() === id.toString()
   );
   if (!foundModel) {
