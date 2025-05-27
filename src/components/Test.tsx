717359c3959a2f505a31dd8558bf520eba1de3ab
yy933 <emilyyu025@gmail.com>
2025-05-27 11:50:28 +0800

    docs: remove Test.tsx

--- src/components/Test.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Test.tsx
-import type { PillProps } from "@/types";
-
-const TestPill = ({ children, className }: PillProps) => (
-  <div className={className}>{children}</div>
-);
-
-export default TestPill;
